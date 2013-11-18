648bf194bd3ad3d3d9d655c4ae51df98d7be97e1
Sergiu Daniel <marsavela@gmail.com>
2013-11-18 16:04:25 +0100

    Volvemos a probar. Parece que el .gitignore funciona bien. Yuju.

--- src/adm/werock/sportstats/MainActivity.java
+++ src/adm/werock/sportstats/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends Activity {
         return true;
         
         
-        // Fuck all of you bitch!!!
+        // Fuck all of you BITCHEEEEESSSSs!!!
     }
     
 }
