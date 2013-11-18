bae50103c9e120f3f4110faa97831637da90942c
Patrick <patmonsi@gmail.com>
2013-11-18 21:52:27 +0100

    Añadido logotipo a la actividad principal

--- src/adm/werock/sportstats/MainActivity.java
+++ src/adm/werock/sportstats/MainActivity.java
@@ -10,7 +10,6 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Dale a tu cuerpo alegria macarena.
     }
 
 
@@ -18,8 +17,6 @@ public class MainActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-        
+        return true;   
     }
-    
 }
