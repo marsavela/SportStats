6eb615f47da136fc5a16805018c790d0a8464893
Patrick <patmonsi@gmail.com>
2013-12-13 11:09:23 +0100

    Login with Facebook

--- src/adm/werock/sportstats/RegisterActivity.java
+++ src/adm/werock/sportstats/RegisterActivity.java
@@ -1,7 +1,21 @@
 package adm.werock.sportstats;
 
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import android.content.Intent;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.FacebookException;
+import com.facebook.Request;
+import com.facebook.Response;
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.model.GraphUser;
+import com.facebook.widget.LoginButton;
+import com.facebook.widget.LoginButton.OnErrorListener;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -75,9 +89,53 @@ public class RegisterActivity extends Activity{
 				// TODO Auto-generated method stub
 			}
 		});
+		
+	//	final TextView txtSaludo = (TextView)findViewById(R.id.txtSaludo);
+		  
+		LoginButton authButton = (LoginButton) findViewById(R.id.login_button);
+	      authButton.setOnErrorListener(new OnErrorListener() {
+	       
+	       @Override
+	       public void onError(FacebookException error) {
+	         
+	       }
+	    });
+	      
+	    authButton.setReadPermissions(Arrays.asList("basic_info","email"));
+	  
+	    authButton.setSessionStatusCallback(new Session.StatusCallback() {
+	         
+	        
+			@Override
+	         public void call(Session session, SessionState state, Exception exception) {
+	          
+	          if (session.isOpened()) {  
+	        	  		Request.newMeRequest(session,
+	                            new Request.GraphUserCallback() {
+	                                @Override
+	                                public void onCompleted(GraphUser user,Response response) {
+	                                    if (user != null) {
+	                     //                 txtSaludo.setText("!Bienvenido " + user.getFirstName() + "!");   
+	                                      Toast.makeText(null, user.getFirstName(), Toast.LENGTH_LONG).show();
+	                                    }
+	                                }
+	                            });
+	          }else if(session.isClosed()) {
+	//        	  txtSaludo.setText("!Bienvenido!");
+	          }
+	         }
+	        });
 	
 	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		Session.getActiveSession().onActivityResult(this, requestCode,
+				resultCode, data);
 	
+	}
+
 	public static boolean isEmailValid(String email) {
 	    boolean isValid = false;
 
